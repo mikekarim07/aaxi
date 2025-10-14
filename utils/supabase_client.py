d691ed4e82c2e840302d4cb4a8188edabb6950ae
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2025-10-14 11:22:56 -0600

    Update supabase_client.py

--- utils/supabase_client.py
+++ utils/supabase_client.py
@@ -1,1 +1,10 @@
+from supabase import create_client
+import streamlit as st
+
+@st.cache_resource
+def init_supabase():
+    """Inicializa la conexión con Supabase."""
+    url = st.secrets["url"]
+    key = st.secrets["key"]
+    return create_client(url, key)
 
